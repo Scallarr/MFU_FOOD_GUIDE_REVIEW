5a26f0bde80a09a685c2dded898c95dd2f922b7e
Scallarr <Kasiditkosit@gmail.com>
2025-08-08 01:54:27 +0700

    Thread

--- back/index.js
+++ back/index.js
@@ -947,7 +947,7 @@ app.get('/all_threads/:userId', async (req, res) => {
       SELECT 
         T.Thread_ID, T.message, T.created_at, T.User_ID,
         U.fullname, U.username,
-        P.profile_picture_url,
+        P.picture_url,
         (SELECT COUNT(*) FROM Thread_Likes WHERE Thread_ID = T.Thread_ID) AS total_likes,
         (SELECT COUNT(*) FROM Thread_reply WHERE Thread_ID = T.Thread_ID) AS total_comments,
         EXISTS (
